19b2552e71309d2dbb5d9be33325c8ae69682189
Ahmed <ahmedamet6069@gmail.com>
2026-03-07 15:45:29 +0100

    Added text prompts to guide the user.

--- CoffeePairing.py
+++ CoffeePairing.py
@@ -3,9 +3,8 @@ import csv
 import random
 import copy
 import os
-#Firt-Feature
 
-#Function to always have correct inputs
+#Function to always have correct integer inputs
 def read_integer(prompt):
     while True:
         try:
@@ -20,17 +19,26 @@ participants_csv = "Coffee Partner Lottery participants.csv"
 #Here you can choose what delimiter the csv uses
 DELIMITER=','
 
-# load participant's data
+# load participant's data from csv and clean if neccesary
 formdata = pd.read_csv(participants_csv, sep=DELIMITER)
 formdata = formdata.drop(["ID"], axis = 1)
-Total_group = len(formdata)
-print(Total_group)
 
-#group size input = x
-GS = read_integer("Please enter the group size: ")
+#Greet the user and explain what this game is
+print(f"""\n ====HOMIES MEET UP====
+\nHi there, and welcome to homies meet up!
+\nUsing this program random groups are generated of the currently signed up people. 
+""")
+
+#Show the current groupsize and ask how big you want the groups to be
+Total_group = len(formdata)
+print(f"""\nThe total amount of signed up people are: {Total_group}""")
+GS = read_integer("Please enter how large you want each group to be: ")
+
+#Calculate the amount of groups and the remainder
 n_groups = Total_group//GS
 r_groups = Total_group%GS
 
+#Spilt the people up in random groups which are stored in dataframes. the dataframes are stored in a dictionary. 
 i = 1
 df_groups = {}
 
@@ -40,13 +48,21 @@ while len(formdata) > r_groups:
     formdata = formdata.drop(index=homies.index)
     i += 1
 
+#Ask how you want the remainders to be split up and split them up
+print(f"""\n there are {r_groups} people remaining how do you want to split them up? 
+\n1. Randomly asign them to the full groups.
+2. Create a new group of the remaining people. """)
+
+rem_split = read_integer("Please make a choice: ")
+
+'''
 if r_groups > 0:
     if r_groups < GS/2:
         
     else:
         i = +1
         df_groups[f"group {i}"] = formdata
-
+'''
 
 print(f"\n{df_groups["group 1"]}")
 print(f"\n{df_groups["group 2"]}")
